9e527a165ea6078f32534442184581755647d5ee
tjforryan <tjforryan@yahoo.co.uk>
2018-07-07 18:49:15 +0100

    Made client get its map from the server, via websockets

--- client/src/Battle/index.js
+++ client/src/Battle/index.js
@@ -1,66 +1,41 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 
 import BattleMap from './BattleMap';
 
-const tempData = [
-  [
-    {},
-    {},
-    {},
-    {},
-    {},
-  ],
-  [
-    {
-      state: 'attackable',
-    },
-    {
-      state: 'destination',
-    },
-    {
-      heroImage: 'sirHector.jpg', 
-      state: 'selected',
-    },
-    {
-      state: 'destination',
-    },
-    {
-      state: 'attackable',
-    },
-  ],
-  [
-    {},
-    {},
-    {},
-    {},
-    {},
-  ],
-  [
-    {},
-    {},
-    {},
-    {},
-    {},
-  ],
-  [
-    {},
-    {},
-    {},
-    {},
-    {},
-  ],
-];
-
 class Battle extends Component {
+  constructor() {
+    super();
+    this.state = { map: null };
+  }
+
+  componentDidMount() {
+    const socket = new WebSocket('ws://localhost:8080');
+    socket.addEventListener('message', (event) => {
+      try {
+        const mapData = JSON.parse(event.data);
+
+        console.log('Recieved map data: ', mapData);
+        this.setState({
+          map: mapData,
+        })
+      } catch (e) {
+        console.log('Could not read map data!')
+      }
+    });
+  };
+
   render() {
     // TODO: Surrounding UI
     return (
       <div>
-        <BattleMap
-          grid={tempData}
-          maxSize={75}
-        />
+        {
+          this.state.map ?
+          <BattleMap
+            grid={this.state.map}
+            maxSize={75}
+          /> :
+          'Waiting for map data...'
+        }
       </div>
     );
   }
--- server/simpleWebSocket.js
+++ server/simpleWebSocket.js
@@ -4,14 +4,68 @@ const wsPort = 8080;
 
 const wss = new WebSocket.Server({ port: wsPort });
 
+const tempData = [
+  [
+    {},
+    {},
+    {},
+    {},
+    {},
+  ],
+  [
+    {
+      state: 'attackable',
+    },
+    {
+      state: 'destination',
+    },
+    {
+      heroImage: 'sirHector.jpg', 
+      state: 'selected',
+    },
+    {
+      state: 'destination',
+    },
+    {
+      state: 'attackable',
+    },
+  ],
+  [
+    {},
+    {},
+    {},
+    {},
+    {},
+  ],
+  [
+    {},
+    {},
+    {},
+    {},
+    {},
+  ],
+  [
+    {},
+    {},
+    {},
+    {},
+    {},
+  ],
+];
+
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    console.log(`Whole message: ${message}`);
-    console.log(`Message type: ${typeof message}`);
-    console.log(`Parsed content: ${JSON.parse(message).data}`);
+    try {
+      const receivedData = JSON.parse(message).data;
+      console.log(`Parsed content: ${receivedData}`);
+      ws.send(JSON.stringify({ something: 'new' }));
+    } catch (e) {
+      console.log('Errored with: ', e)
+      ws.close(1003, 'Received data not supported!');
+    }
   });
 
-  ws.send('something');
+  ws.send(JSON.stringify(tempData));
 });
 
 console.log(`Listening using WS on port: ${wsPort}`)
