d514cd057a52e62d7ef8dcfc5edc87eb9ef02b3c
Tom Forryan <tjforryan@yahoo.co.uk>
2018-11-23 21:34:52 +0000

    Tidied up props used for MapSquare test

--- src/client/Battle/MapSquare.test.jsx
+++ src/client/Battle/MapSquare.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MapSquare from './MapSquare';
 
-const MIN_PROPS = { cell: {} };
+const MIN_PROPS = { cell: {}, x: 1, y: 1 };
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
